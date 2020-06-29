0a386d9f5d3b824f21d97f7f761285e31b8dd240
Maxim Leontiev <maxim.leont@gmail.com>
2020-06-29 15:16:05 +0700

    fix: removed i18next global instance dependecie

--- src/index.js
+++ src/index.js
@@ -1,52 +1,38 @@
-import i18next from 'i18next';
-
 export default class I18nEndings {
-	
 	type = 'postProcessor';
 	name = 'endings';
 	search = /\[\[[0-9\.]+(\|.[^\[]+)+\]\]/gm;
-	
 	
 	constructor(options) {
 		this.options = options;
 	}
 	
-	
-	static formatter(num, endings) {
+	getVariant(number, values, translator) {
+		if (this.options && typeof this.options[translator.language] === 'function') {
+			return this.options[translator.language](number, values);
+		}
+		
 		let textCase = 0;
 		
 		try {
-			textCase = i18next.services.pluralResolver.getRule(i18next.language).plurals(num)
+			textCase = translator.pluralResolver.getRule(translator.language).plurals(number);
 		} catch (e) {
-			console.log(e);
 		}
 		
-		return endings[textCase];
+		return values[textCase];
 	}
-	
-	
-	getVariant(number, values, lang) {
-		let fn = I18nEndings.formatter;
-		
-		if ( this.options && typeof this.options[lang] === 'function' ) {
-			fn = this.options[lang];
-		}
-		
-		return fn(number, values);
-	}
-	
 	
 	process(value, key, options, translator) {
-		
-		if ( this.search.test(value) ) {
+		if (this.search.test(value)) {
 			const inclinations = value.match(this.search);
 			
-			inclinations.map(inc => {
+			console.log(translator);
+			inclinations.map((inc) => {
 				const values = inc.replace('[[', '').replace(']]', '').split('|');
 				const number = parseFloat(values[0], 10);
 				
 				values.splice(0, 1);
-				const variant = this.getVariant(number, values, translator.lang);
+				const variant = this.getVariant(number, values, translator);
 				
 				value = value.replace(inc, variant);
 			});
